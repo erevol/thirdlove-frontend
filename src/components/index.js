7078ac981dc899c2583859fce22d55453bd600a7
Eugenia Revol <euge.revol@gmail.com>
2019-05-06 01:20:22 -0300

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header";
-export { default as ProductDescription } from "./ProductDescription";
-export { default as ProductForm } from "./ProductForm";
-export { default as Slider } from "./Slider";
-export { default as Thumbnail } from "./Thumbnail";
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './assets/favicon.ico';
-import './assets/scss/global.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
